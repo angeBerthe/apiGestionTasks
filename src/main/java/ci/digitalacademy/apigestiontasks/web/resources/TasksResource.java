70f31684cc00d820ce4d3e4451e47e7886e183ee
Kongo Abdoul <abdoulmalickkongo@gmail.com>
2024-09-17 18:59:05 +0000

    Ajout des fonctionnalités de Tasks.

--- src/main/java/ci/digitalacademy/apigestiontasks/web/resources/TasksResource.java
+++ src/main/java/ci/digitalacademy/apigestiontasks/web/resources/TasksResource.java
@@ -2,9 +2,15 @@ package ci.digitalacademy.apigestiontasks.web.resources;
 
 import ci.digitalacademy.apigestiontasks.services.ProjectService;
 import ci.digitalacademy.apigestiontasks.services.TasksService;
+import ci.digitalacademy.apigestiontasks.services.TeamService;
 import ci.digitalacademy.apigestiontasks.services.dto.ProjectDTO;
 import ci.digitalacademy.apigestiontasks.services.dto.TasksDTO;
+import ci.digitalacademy.apigestiontasks.services.dto.TeamDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -22,21 +28,33 @@ public class TasksResource {
 
     private final TasksService tasksService;
     private final ProjectService projectService;
+    private final TeamService teamService;
 
     @PostMapping
     @ApiResponse(responseCode = "201", description = "REST, request to save a task")
+    @Operation(summary = "Save a new task", description = "This endpoint allow to save tasks")
     public ResponseEntity<?> save(@RequestBody TasksDTO tasks) {
         log.debug("Rest request to save Tasks : {}", tasks);
         Optional<ProjectDTO> project = projectService.findOne(tasks.getProject().getId());
+        Optional<TeamDTO> team = teamService.findOne(tasks.getTeam().getId());
         if (project.isPresent()) {
             tasks.setProject(project.get());
-            return new ResponseEntity<>(tasksService.saveTasks(tasks), HttpStatus.CREATED);
+            if (team.isPresent()) {
+                tasks.setTeam(team.get());
+                return new ResponseEntity<>(tasksService.saveTasks(tasks), HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>("Id of team not found", HttpStatus.NO_CONTENT);
+            }
         } else {
             return new ResponseEntity<>("Id of project not found" ,HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/{id}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Request to get tasks by id"),
+            @ApiResponse(responseCode = "404", description = "School not found", content = @Content(schema = @Schema(implementation = String.class)))
+    })
     public ResponseEntity<?> getTasksById(@PathVariable Long id) {
         log.debug("Rest request to get Tasks by id : {}", id);
         Optional<TasksDTO> tasks = tasksService.findOne(id);
