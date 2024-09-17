d90d3ec9f52be2c5e7577ecf72c80fb661568c34
AboubacarSiriki <sidiksoum344@gmail.com>
2024-09-17 16:32:18 +0000

    notification

--- src/main/java/ci/digitalacademy/apigestiontasks/services/NotificationService.java
+++ src/main/java/ci/digitalacademy/apigestiontasks/services/NotificationService.java
@@ -1,4 +1,17 @@
 package ci.digitalacademy.apigestiontasks.services;
 
+import ci.digitalacademy.apigestiontasks.services.dto.NotificationDTO;
+import ci.digitalacademy.apigestiontasks.services.dto.ProjectDTO;
+
+import java.util.List;
+
 public interface NotificationService {
+
+    NotificationDTO save(NotificationDTO notificationDTO);
+
+    List<NotificationDTO> findAll();
+
+    void delete(Long id);
+
+    NotificationDTO saveProject(ProjectDTO projectDTO);
 }
